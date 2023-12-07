d59a9dbf067b0b29e81795e67c99cebd21869e2c
dipakpatil33 <76866520+dipakpatil33@users.noreply.github.com>
2023-12-07 16:09:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 #--to format data in table (json to table)
 
